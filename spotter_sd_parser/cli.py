0d06b01c28c96b160b9cf2851cc3f412a96597a6
Randall Pittman <pittmara@oregonstate.edu>
2025-05-07 16:05:52 -0700

    Remove old smartmooring processing

--- spotter_sd_parser/cli.py
+++ spotter_sd_parser/cli.py
@@ -1,8 +1,7 @@
 from cyclopts import App
 
-from spotter_sd_parser import sd_file_parser, smartmooring, smartmooring_pd
+from spotter_sd_parser import sd_file_parser, smartmooring_pd
 
 app = App(name="spotter-sd-parser", help_on_error=True)
 app.default(sd_file_parser.main)
-app.command(smartmooring.main, name="smartmooring")
 app.command(smartmooring_pd.main, name="smartmooring-pd")
